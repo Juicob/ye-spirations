f3a88d453e15c6e0f01058f0ded5c94153ed2e8a
erodsn <rafael.carrasco@flatironschool.com>
2020-11-05 16:02:12 -0600

    adding placeholder options

--- components/query_input.py
+++ components/query_input.py
@@ -1,11 +1,14 @@
 import dash_bootstrap_components as dbc
 import dash_html_components as html
+import random
 
 
-query_input = html.Div(
-    [
-        dbc.Input(id="query-input", placeholder="What's Your Mood?", type="text"),
-        html.Br(),
-        html.P(id="output"),
-    ]
-)
+def get_query_input(placeholder):
+    query_input = html.Div(
+        [
+            dbc.Input(id="query-input", placeholder=placeholder, type="text"),
+            html.Br(),
+            html.P(id="output"),
+        ]
+    )
+    return query_input
