41a03a6c2fee156c5725dc25f736994e8bd3696b
erodsn <rafael.carrasco@flatironschool.com>
2020-11-05 16:04:11 -0600

    adding placeholder changes

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import dash
 
+import random
+
 import dash_core_components as dcc
 import dash_bootstrap_components as dbc
 import dash_html_components as html
@@ -7,6 +9,7 @@ import dash_html_components as html
 from dash.dependencies import Input, Output, State
 
 from components.button import button
+from components.query_input import get_query_input
 
 external_stylesheets = dbc.themes.LUX
 
@@ -19,7 +22,9 @@ server = app.server
 app.layout = html.Div(children=[
     html.Div(className="big-container",
              children=[
+                 dcc.Interval(id="app-interval-timer", interval=3.6e6, n_intervals=0),
                  html.H1("Ye-Spirations"),
+                 html.Div(id="query-input-container", children=[]),
                  button,
                  html.Br(),
                  html.Div(id="image-container",
@@ -38,6 +43,18 @@ def return_on_click(n_clicks):
         message = f"You have clicked the button\n{n_clicks} times"
         return html.H3(message)
 
+# comment
+@app.callback(Output(component_id="query-input-container", component_property="children"),
+              [Input(component_id="app-interval-timer", component_property="n_intervals")])
+def refresh_placeholder(n_intervals):
+    placeholders = ["what's your vibe?",
+                    "how you feeling?",
+                    "where do you want to go?",
+                    "where's your favorite memory?",
+                    "what's your favorite drug?"]
+    placeholder = random.choice(placeholders)
+    query_input = get_query_input(placeholder=placeholder)
+    return query_input
 
 if __name__ == "__main__":
     app.run_server('localhost', debug=True)
