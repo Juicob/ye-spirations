4a8aa05a84968e583fceacf1cc2c11543a780681
Juico Bowley <juico.s.bowley@gmail.com>
2020-11-05 21:40:02 -0500

    updated functions, moved files

--- backend/functions.py
+++ backend/functions.py
@@ -12,26 +12,26 @@ with open('../secrets.json') as f:
 api = Api(auth)
 
 def get_rando_line():
-    with open(r'./Kanye West Lyrics.txt', encoding='utf8') as f:
+    with open(r'../data/Kanye West Lyrics.txt', encoding='utf8') as f:
         lines = f.readlines()
         not_blank_or_descriptive_lines = [line for line in lines if len(line) > 1 and '[' not in line]
-        rando_line = print(not_blank_or_descriptive_lines[random.randrange(0,len(not_blank_or_descriptive_lines))])
+        rando_line = not_blank_or_descriptive_lines[random.randrange(0,len(not_blank_or_descriptive_lines))]
+        print(rando_line)
         return rando_line
     
 def get_rando_photo(search_term, orientation='landscape'):
-    photo = api.photo.random(1, query=search_term, orientation=orientation)[0].urls.regular
-    response = requests.get(photo, stream=True)
+    request = api.photo.random(1, query=search_term, orientation=orientation)[0].urls.regular
+    response = requests.get(request, stream=True)
     response.raw.decode_content = True
-    rando_photo = Image.open(response.raw)
+    rando_photo = Image.open(response.raw) #.convert("RGBA")
     return rando_photo
 
-def put_line_on_photo(line, photo, text_size=30):
-    font_type = ImageFont.truetype(font='arial.ttf', size=text_size)
+def put_line_on_photo(rando_line, photo, text_size=30):
+    print(rando_line, photo)
+    font_type = ImageFont.load_default()#.truetype(font='arial.ttf', size=text_size)
     draw = ImageDraw.Draw(photo)
     draw.text(xy=(50,150), 
          text=rando_line, 
-         # Issue with undefined variable. I figure it's a simple thing that I'm just not seeing 
-         # right now soI'm using that as a sign to switch over and try to finish some curriculum=D
          fill=(150,105,150,50), 
          font=font_type)
     return photo
